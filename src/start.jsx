0c7869d14f5ccee775dcbc3f9d6ff6fdb95b8fc8
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-06-08 15:53:11 +0000

    deploy: 57522ab95fb59179b50b3b4111d22bac34570a2f

---
+++ src/start.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './assets/sass/modules/app.module.scss'
+import { Join } from "./assets/util/JoinClasses";
+import ReactFacts from "./projects/Basic/React-Facts/ReactFacts";
+
+export default function Start(){
+    return (
+        <div>
+            <ReactFacts />
+            {/* You can now start coding right here..*/}
+        </div>
+    )
+}
+// this will serve as the APP.JS 
